fabd98ee08de040bba27cfd2cd68f7f7666496ec
Victor Boyko <boyko.victor@gmail.com>
2015-06-05 18:13:29 +0200

    103B complete

--- src/com/codeforces/victor/Problem103B.java
+++ src/com/codeforces/victor/Problem103B.java
@@ -47,17 +47,55 @@ public class Problem103B {
 			getModifiable(key).remove(value);
 			getModifiable(value).remove(key);
 		}
+		
+		@Override
+		public String toString() {
+			return map.toString();
+		}
+	}
+	
+	static class IntW {
+		public IntW(int i) {
+			this.i = i;
+		}
+		int i;
+		@Override
+		public String toString() {
+			return Integer.toString(i);
+		}
 	}
 
+	private static boolean isItKtulhu(Integer cameFrom, Integer current, Graph g, boolean[] visited, IntW loopFaced) {
+		visited[current] = true;
+		//g.remove(cameFrom, current);
+		for(Integer i : g.get(current)) {
+			if (i == cameFrom) continue;
+			if (visited[i]) {
+				if (loopFaced.i >= 2) 
+					return false; 
+				else loopFaced.i++;
+				continue;
+			}
+			if (!isItKtulhu(current, i, g, visited, loopFaced)) return false;
+		}
+		return current != 0 || loopFaced.i == 2;
+	}
+	
 	public static void implementation(InputStream in, PrintStream out)
 			throws Exception {
 		Scanner s = new Scanner(in);
 		int n = s.nextInt();
 		int m = s.nextInt();
-		Graph g = new Graph(n);
+		Graph g = new Graph(n+1);
 		Stream.iterate(1, i -> i + 1).limit(m)
 				.forEach(i -> g.put(s.nextInt(), s.nextInt()));
-		System.out.println("OUT!");
+		boolean[] visited = new boolean[n+1];
+		g.put(0, 1);
+		visited[0] = true;
+		boolean ktulhuSpotted = isItKtulhu(0, 0, g, visited, new IntW(0));
+		for(boolean b : visited)
+			ktulhuSpotted &= b;
+		out.println(ktulhuSpotted ? "FHTAGN!" : "NO");
 	}
 
 	public static void main(String[] args) throws Exception {
